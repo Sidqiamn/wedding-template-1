97a29e38f7cd6e9d3f0ca35af0722cea1fe2cef5
qiamn <89834713+Sidqiamn@users.noreply.github.com>
2025-04-09 21:30:41 +0700

    add

--- src/components/halaman2.jsx
+++ src/components/halaman2.jsx
@@ -3,6 +3,8 @@ import image1 from "../assets/image1.jpeg";
 import image2 from "../assets/image2.jpeg";
 import image3 from "../assets/image3.jpeg";
 import ig from "../assets/ig.png";
+import image4 from "../assets/imag4.jpg";
+import image5 from "../assets/image5.png";
 
 const Halaman2 = () => {
   // 🎯 Target waktu (ubah sesuai kebutuhanmu)
@@ -102,14 +104,47 @@ const Halaman2 = () => {
         <img src={image3} alt="" className="w-full h-auto" />
         <div className="absolute bottom-4 w-full flex justify-center">
           <div className="bg-gray-300 bg-opacity-40 text-white px-4 py-3 rounded flex flex-col  gap-3 mx-5 ">
-            <h1 className="text-4xl">Sidqi</h1>
-            <h2 className="text-lg">Sidqi Amanullah</h2>
+            <h1 className="text-4xl">Aman</h1>
+            <h2 className="text-lg">Sidqia Amanull</h2>
             <div className="flex items-center gap-2">
               <img src={ig} alt="" className="w-5 h-5" />
-              <p>@sidqiaman</p>
+              <p>@amansidqi</p>
             </div>
             <hr className="w-full border-white/30" />
             <p>Putra dari bapak lord thanos dan queen bumi</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="text-center mt-10 mx-5 text-xs">
+          "Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan
+          pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung
+          merasa tenteram kepadanya. Dan Dia menjadikan di antaramu rasa kasih
+          dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat
+          tanda-tanda kebesaran Allah bagi kaum yang berpikir"
+        </p>
+        <div className="flex items-center justify-center gap-4 mt-5">
+          <hr className="w-20 border-t border-gray-400" />
+          <p className="text-gray-700 uppercase tracking-widest font-bold text-xs">
+            ar-rum
+          </p>
+          <hr className="w-20 border-t border-gray-400" />
+        </div>
+      </div>
+      <div className="mt-10 relative">
+        <img src={image4} alt="" className="opacity-30" />
+
+        <div className="absolute top-0 flex mt-10 justify-center items-center flex-col w-full">
+          <p className="text-4xl font-light">
+            Save <p className="inline font-allura">The</p> Date
+          </p>
+          <p className="text-xs text-center mt-10">
+            Dengan Memohon rahmat dan ridho Allah SWT, Kami Mengundang
+            Bapak/Ibu/Saudara/i, untuk menghindari acara pernikahan Kami.
+            insyaAllah kami akan menyelenggarakan acara pernikahan :{" "}
+          </p>
+          <div className="mx-20 mt-6">
+            <img src={image5} alt="" />
           </div>
         </div>
       </div>
